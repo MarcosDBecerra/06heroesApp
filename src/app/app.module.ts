cc8327dafabfb02623a7c1b8b03c154b38b5dbbb
MarcosDBecerra <161252884+MarcosDBecerra@users.noreply.github.com>
2024-03-24 18:12:54 -0300

    card heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { MaterialModule } from './material/material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    HttpClientModule,
 
     MaterialModule
   ],
